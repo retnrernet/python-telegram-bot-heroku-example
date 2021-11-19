cdb401b39072340494cc691a47805a3349154769
retnrernet <49436790+retnrernet@users.noreply.github.com>
2021-11-19 22:24:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,15 +9,15 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
-mode = os.getenv("MODE")
-TOKEN = os.getenv("TOKEN")
+mode = "prod"
+TOKEN = "1023713845:AAGZ-hHJbPfx8phFn53udo1wYVCxSVfOFH8"
 if mode == "dev":
     def run(updater):
         updater.start_polling()
 elif mode == "prod":
     def run(updater):
         PORT = int(os.environ.get("PORT", "8443"))
-        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+        HEROKU_APP_NAME = "petrjela"
         updater.start_webhook(listen="0.0.0.0",
                               port=PORT,
                               url_path=TOKEN)
